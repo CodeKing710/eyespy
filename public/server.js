0aae6195d7d3039a4911cc0577cf573c44017560
Carter Ervin <cjrox01@yahoo.com>
2022-05-23 20:09:27 -0500

    Project Creation (bash::nodeproj)

---
+++ public/server.js
@@ -0,0 +1,14 @@
+(async () => {
+  require('dotenv').config();
+
+  const express = require('express');
+  const ejs = require('ejs');
+  const app = express();
+  const PORT = process.env.PORT ?? 3000;
+
+  app.get('*', (req, res) => {
+    res.status(404).render('404');
+  });
+
+  app.listen(PORT, console.log(`Listening on port ${PORT}...`));
+})();
