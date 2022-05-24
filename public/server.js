cbc580cabec6d576d6ee18ae59f2a8e85bf11c50
Carter Ervin <cjrox01@yahoo.com>
2022-05-24 09:20:13 -0500

    Project Creation (bash::nodeproj)

--- public/server.js
+++ public/server.js
@@ -6,6 +6,9 @@
   const app = express();
   const PORT = process.env.PORT ?? 3000;
 
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}));
+
   app.get('*', (req, res) => {
     res.status(404).render('404');
   });
