75f187bac7d6f92c681cb4b9af2a6edb3e084726
Carter Ervin <cjrox01@yahoo.com>
2022-05-26 15:48:25 -0500

    More setup

---
+++ public/js/game.js
@@ -0,0 +1,16 @@
+//Add button listeners
+$('#play').on("click",function(){
+  console.log("play");
+});
+$('#worlds').on("click",function(){
+  console.log("worlds");
+});
+$('#settings').on("click",function(){
+  console.log("settings");
+});
+function show(id) {
+
+}
+function close(id) {
+
+}
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const main = require("express").Router();
 
 //Routes
 main.get('/', (req, res) => {
-  res.status(200).render('home');
+  res.status(200).render('game');
 });
 
 module.exports = main;
